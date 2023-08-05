44a901da85e1a7574314214053f11c42bf1e2c05
tutu <t433567423@163.com>
2023-08-05 12:54:16 +0800

    feat: 用户系统-登录、注册接口

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,62 +1,26 @@
-import { Body, Controller, HttpCode, HttpException, HttpStatus, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpException, HttpStatus, Post, UsePipes, ValidationPipe } from '@nestjs/common'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { UserService } from './user.service'
-import { UserDto } from './user.dto'
+import { UserReqDto } from './user.dto'
 import { md5Password } from '../../utils'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
-import { sign, verify } from 'jsonwebtoken'
-import { PRIVATE_KEY, PUBLIC_KEY } from '../../../sercret'
 
-@Controller()
+@Controller('user')
 @ApiTags('user')
 export class UserController {
   constructor (private readonly UserService: UserService) {
   }
 
-  @Post('/login')
-  @ApiOperation({ summary: '登录注册', description: '注册新登录' })
-  @HttpCode(HttpStatus.OK)
-  @ApiResponse({
-    status: 200,
-    description: '成功返回200，失败返回401',
-    type: UserDto
-  })
-  async login (@Body() userReq: UserDto) {
-    const {
-      username,
-      password: originPassword
-    } = userReq
-    const password = md5Password(originPassword)
-    const ResUser = (await this.UserService.findByPassword(username, password))
-    if (ResUser.length === 0) {
-      throw new HttpException('密码不正确', HttpStatus.FORBIDDEN)
-    }
-    const user = { id: ResUser[0].id, username: ResUser[0].username }
-    // 非对称加密生成token
-    const token = sign(user, PRIVATE_KEY, {
-      expiresIn: 60 * 60 * 24 * 7,
-      algorithm: 'RS256'
-    })// RS256非对称加密(min:2048)、HS256对拆加密(固定密钥)加密
-    console.log('尝试解密\n')
-    console.log(verify(token, PUBLIC_KEY, { algorithms: ['RS256'] }))
-    return {
-      msg: 'success',
-      data: {
-        ...user,
-        token
-      }
-    }
-  }
-
   @Post('/registry')
   @ApiOperation({ summary: '用户注册', description: '注册新用户' })
+  @UsePipes(new ValidationPipe())
   @HttpCode(HttpStatus.OK)
   @ApiResponse({
     status: 200,
     description: '成功返回200，失败返回400',
-    type: UserDto
+    type: UserReqDto
   })
-  async registry (@Body() user: UserDto) {
+  async registry (@Body() user: UserReqDto) {
     const {
       username,
       password: originPassword
--- src/modules/user/user.dto.ts
+++ src/modules/user/user.dto.ts
@@ -1,12 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty } from 'class-validator'
+import { IsNotEmpty, IsString } from 'class-validator'
 
-export class UserDto {
+class resDto {
+  @ApiProperty({ description: 'success' })
+  readonly msg: string
+}
+
+export class UserReqDto {
   @ApiProperty({ example: 'tutu', description: '用户名' })
   @IsNotEmpty({ message: '用户名不能为空' })
+  @IsString()
   readonly username: string
 
   @ApiProperty({ example: '123456', description: '密码' })
   @IsNotEmpty({ message: '密码不能为空' })
     password: string
+}
+
+export class UserResDto extends resDto {
+  @ApiProperty({ description: 'token令牌' })
+  readonly token: string
+
+  @ApiProperty({ description: '用户名' })
+  readonly username: string
+
+  @ApiProperty({ description: '用户id' })
+  readonly id: number
 }
