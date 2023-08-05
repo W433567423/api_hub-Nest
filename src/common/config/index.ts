003a32081f01be4b0083e3ef620c61c5c27eebbc
tutu <t433567423@163.com>
2023-08-05 11:19:59 +0800

    feat: 用户系统-注册接口

--- src/common/config/index.ts
+++
@@ -1,5 +0,0 @@
-export const COS_SECRET_ID = 'AKIDcDneQ6iDh6Rm7MYjv4787146A7ylYJ7o'
-export const COS_SECRET_KEY = 'd5LRN830wtttwx29KZsFf7VvKf8czXwX'
-export const COS_BUCKET_NAME = 'tutu-1313352375'
-export const COS_BUCKET_REGION = 'ap-nanjing'
-export {}
