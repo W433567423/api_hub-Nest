42ea95fc8fa92561a37bc926ed0fbe8d7ffb2501
tutu <t433567423@163.com>
2023-08-13 13:30:29 +0800

    fix: 文件系统-头像获取接口

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,10 @@
-import multer from 'multer'
-import * as path from 'path'
-
-export const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.resolve(__dirname, '../tmp/my-uploads'))
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = String(Date.now()) + '-' + String(Math.round(Math.random() * 1E9))
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
+// export const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.resolve(__dirname, '../tmp/my-uploads'))
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = String(Date.now()) + '-' + String(Math.round(Math.random() * 1E9))
+//     cb(null, file.fieldname + '-' + uniqueSuffix)
+//   }
+// })
 export const SERVER_PORT = 3000
