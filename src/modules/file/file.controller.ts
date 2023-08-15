d6e88c6da9c3e0c8ef8b0608b2deb2eb477f65bd
tutu <t433567423@163.com>
2023-08-15 23:10:52 +0800

    fix: 发布moment的配图接口

--- src/modules/file/file.controller.ts
+++ src/modules/file/file.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Req,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors
 } from '@nestjs/common'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
@@ -69,5 +70,36 @@ export class FileController {
   })
   async getUserAvatar (@Param('userId', ParseIntPipe) userId: number) {
     return await this.FileService.getUserAvatar(userId)
+  }
+
+  @Post('/pictures')
+  @ApiOperation({
+    summary: '用户上传moment的配图'
+  })
+  @UseInterceptors(FileInterceptor('pics'))
+  @HttpCode(HttpStatus.OK)
+  @ApiResponse({
+    status: 200,
+    description: '成功返回200，失败返回400'
+  })
+  async savaMomentPics (@Req() req: IUserReq, @UploadedFiles() pics: Express.Multer.File) {
+    // try {
+    //   await fs.promises.stat('src/../.uploads')
+    // } catch (e) {
+    //   // 不存在文件夹，直接创建 {recursive: true} 这个配置项是配置自动创建多个文件夹
+    //   await fs.promises.mkdir('src/../.uploads', { recursive: true })
+    // }
+    // const filePath = path.resolve('src/../.uploads', String(req.user.id) + String(avatar.fieldname))
+    // fs.writeFileSync(filePath, avatar.buffer)
+    // // console.log(avatar.mimetype, avatar.size, 'avatar' + avatar.fieldname)
+    // const { Location } = await uploadFile({
+    //   Key: `hub/avatar/${req.user.id}-avatar.png`,
+    //   FilePath: filePath
+    // }) as any
+    // const avatarTable = await this.FileService.saveAvatar(Location, avatar.mimetype, String(avatar.size) + 'bit')
+    //
+    // await this.UserService.saveUserAvatar(avatarTable, req.user.id)
+    // return '头像上传成功'
+    return 'ok'
   }
 }
